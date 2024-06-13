74b3611f65b50c357531aca75651a18f83adc749
++ <++>
2024-06-12 20:21:49 -0500

    Working fetch

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-document.addEventListener('DOMContentLoaded', () => console.log("We are connected!")) 
+document.addEventListener('DOMContentLoaded', () => fetchData)
+
+function fetchData() 
+{
+    fetch('https://api.github.com/users/michael-kang')
+   .then((resp) => resp.json())
+   .then((data) => console.log(data))
+}   
